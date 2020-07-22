95b627fef22365d231b26a5b5983fe9bc4617ef8
Marc Ermshaus <marc@ermshaus.org>
2020-07-22 21:25:44 +0200

    Extract shared code from test helpers

--- test/Library/SynchronousClientHelper.php
+++ test/Library/SynchronousClientHelper.php
@@ -7,7 +7,7 @@ namespace Bunny\Test\Library;
 use Bunny\Client;
 use React\Promise\Promise;
 
-final class SynchronousClientHelper
+final class SynchronousClientHelper extends AbstractClientHelper
 {
     /**
      * @param array|null $options
@@ -71,60 +71,9 @@ final class SynchronousClientHelper
      */
     public function getDefaultOptions(): array
     {
-        $uri = getenv('TEST_RABBITMQ_CONNECTION_URI');
-
-        $uriComponents = parse_url($uri);
-
-        if (
-            !isset($uriComponents['scheme'])
-            || !in_array($uriComponents['scheme'], ['amqp', 'amqps'])
-        ) {
-            throw new \RuntimeException(
-                sprintf(
-                    'URI scheme must be "amqp" or "amqps". URI given: "%s"',
-                    $uri
-                )
-            );
-        }
-
         $options = [];
 
-        if (isset($uriComponents['host'])) {
-            $options['host'] = $uriComponents['host'];
-        }
-
-        if (isset($uriComponents['port'])) {
-            $options['port'] = $uriComponents['port'];
-        }
-
-        if (isset($uriComponents['user'])) {
-            $options['user'] = $uriComponents['user'];
-        }
-
-        if (isset($uriComponents['pass'])) {
-            $options['password'] = $uriComponents['pass'];
-        }
-
-        if (isset($uriComponents['path'])) {
-            $vhostCandidate = $uriComponents['path'];
-
-            if (strpos($vhostCandidate, '/') === 0) {
-                $vhostCandidate = substr($vhostCandidate, 1);
-            }
-
-            if (strpos($vhostCandidate, '/') !== false) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'An URI path component that is a valid vhost may not contain unescaped "/" characters. URI given: "%s"',
-                        $uri
-                    )
-                );
-            }
-
-            $vhostCandidate = rawurldecode($vhostCandidate);
-
-            $options['vhost'] = $vhostCandidate;
-        }
+        $options = array_merge($options, $this->parseAmqpUri(getenv('TEST_RABBITMQ_CONNECTION_URI')));
 
         return $options;
     }
