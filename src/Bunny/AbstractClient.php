91c1da21e294f119d2230d051ad91ad5a65635af
Standa Jaša <standa@jasa.name>
2020-09-25 11:40:17 +0200

    Fix: exception on too high heartbeat - the value is a signed int16

--- src/Bunny/AbstractClient.php
+++ src/Bunny/AbstractClient.php
@@ -131,8 +131,8 @@ abstract class AbstractClient
 
         if (!isset($options["heartbeat"])) {
             $options["heartbeat"] = 60.0;
-        } elseif ($options['heartbeat'] >= 2**16) {
-            throw new InvalidArgumentException('Heartbeat too high: the value is int16.');
+        } elseif ($options["heartbeat"] >= 2**15) {
+            throw new InvalidArgumentException("Heartbeat too high: the value is a signed int16.");
         }
 
         if (is_callable($options['heartbeat_callback'] ?? null)) {
