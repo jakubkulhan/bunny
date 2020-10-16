ab3b0dec30128339e8aa9c5646b9c6ab8a7d8d94
tatikoma <me@tati.pro>
2020-10-16 16:03:28 +0300

    also add disconnect for resource-related errors

--- src/Bunny/AbstractClient.php
+++ src/Bunny/AbstractClient.php
@@ -310,11 +310,13 @@ abstract class AbstractClient
             $info = stream_get_meta_data($this->stream);
 
             if (isset($info["timed_out"]) && $info["timed_out"]) {
+                $this->disconnect(Constants::STATUS_RESOURCE_ERROR, "Connection closed by server unexpectedly");
                 throw new ClientException("Timeout reached while reading from stream.");
             }
         }
 
         if (@feof($this->stream)) {
+            $this->disconnect(Constants::STATUS_RESOURCE_ERROR, "Connection closed by server unexpectedly");
             throw new ClientException("Broken pipe or closed connection.");
         }
 
