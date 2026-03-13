ebd0f4537a92e466f4dfe96ad9fe973ec1549935
Cees-Jan Kiewiet <ceesjank@gmail.com>
2026-03-13 23:23:46 +0100

    [0.6.x] CS fixes for the Channel and Client mocks

--- test/MockClientInterface.php
+++ test/MockClientInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bunny\Test;
 
-
 use Bunny\ChannelInterface;
 use Bunny\ClientInterface;
 
@@ -17,7 +16,6 @@ final class MockClientInterface implements ClientInterface
     {
         $this->isConnected = $isConnected;
     }
-
 
     public function channel(): ChannelInterface
     {
