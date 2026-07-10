93a6a0b3a0ab5bdc5db63a5b02846f186b3c9182
Tomáš Novotný <tomas.novotny@biano.com>
2026-07-10 12:04:44 +0200

    Refactor tests

--- test/Library/Client.php
+++ test/Library/Client.php
@@ -4,27 +4,27 @@ declare(strict_types=1);
 
 namespace Bunny\Test\Library;
 
-use Bunny\Client;
+use Bunny\Client as BunnyClient;
 use function array_merge;
 
-final class ClientHelper
+final class Client
 {
     /**
      * @param array<string, mixed>|null $options
      */
-    public function createClient(?array $options = null): Client
+    public static function createClient(?array $options = null): BunnyClient
     {
-        $options = array_merge($this->getDefaultOptions(), $options ?? []);
+        $options = array_merge(self::getDefaultOptions(), $options ?? []);
 
-        return new Client($options);
+        return new BunnyClient($options);
     }
 
     /**
      * @return array<string, mixed>
      */
-    public function getDefaultOptions(): array
+    public static function getDefaultOptions(): array
     {
-        $options = [];
+        $options = ['heartbeat' => 1.0];
 
         $options = array_merge($options, parseAmqpUri(Environment::getTestRabbitMqConnectionUri()));
 
